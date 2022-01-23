ecd0903746b480253cc3b3bfb0479fd544fcac9f
slavi <shimmy@abv.bg>
2022-01-23 15:07:14 +0200

    Location search

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const API_KEY = "aa524c2fce727dda3d67deec1756cbae";
+
+
+document.getElementById("location").addEventListener("submit", event => {
+    event.preventDefault();
+
+    document.getElementById("unknown_city_error").classList.add("d-none");
+
+
+    const city = document.getElementById("city").value;
+
+    if(city === ""){
+        return;
+    }
+
+
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`)
+        .then(result => result.json())
+        .then(result => {
+            if(result.length === 0){
+                document.getElementById("unknown_city_error").classList.remove("d-none");
+            }            
+        })
+})
+
