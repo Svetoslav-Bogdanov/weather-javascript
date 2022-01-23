911a550a853a41855674de27306f2d804ac97d06
slavi <shimmy@abv.bg>
2022-01-23 16:05:11 +0200

    Display locations list with  javascript

--- index.js
+++ index.js
@@ -4,22 +4,71 @@ const API_KEY = "aa524c2fce727dda3d67deec1756cbae";
 document.getElementById("location").addEventListener("submit", event => {
     event.preventDefault();
 
-    document.getElementById("unknown_city_error").classList.add("d-none");
+    hideUknownCityError();
 
+    resetLocationBlock();
 
     const city = document.getElementById("city").value;
 
-    if(city === ""){
+    if (city === "") {
         return;
     }
 
+    fetchAddLocations(city);
 
+})
+
+function resetLocationBlock() {
+
+    document.getElementById("location_block").classList.add("d-none");
+
+    document.getElementById("location_list").innerHTML = "";
+}
+
+function hideUknownCityError() {
+    document.getElementById("unknown_city_error").classList.add("d-none");
+}
+
+function showUnknownCityError() {
+    document.getElementById("unknown_city_error").classList.remove("d-none");
+}
+
+function displayLocationsList(result) {
+    let locationsList = "";
+
+    result.forEach(element => {
+        locationsList += `<li class="list-group-item" data-lat="${element.lat}" data-lon="${element.lon}">${element.name}, ${element.country}</li>`
+        
+    });
+
+    document.getElementById("location_list").innerHTML = locationsList;
+
+    document.getElementById("location_block").classList.remove("d-none");
+}
+
+function fetchAddLocations(city) {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`)
         .then(result => result.json())
         .then(result => {
-            if(result.length === 0){
-                document.getElementById("unknown_city_error").classList.remove("d-none");
-            }            
+            if (result.length === 0) {
+
+                showUnknownCityError();
+
+                return;
+            }
+
+            displayLocationsList(result);
+
         })
+}
+
+document.getElementById("location_list").addEventListener("click", event => {
+    if(event.target.tagName != "LI"){
+        return
+    }     
+    console.log(event.target.dataset)
 })
 
+function searchWeather(coordinates){
+    
+}
